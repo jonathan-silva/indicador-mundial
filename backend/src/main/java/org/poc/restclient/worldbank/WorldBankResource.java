dfba888df380f86f1340e88afe7850973db12eef
Jonathan SIlva <jonathan.costa@lifecon.com.br>
2021-02-06 14:53:57 -0300

    feat: adicionado modulo do backend

---
+++ backend/src/main/java/org/poc/restclient/worldbank/WorldBankResource.java
@@ -0,0 +1,53 @@
+package org.poc.restclient.worldbank;
+
+import jdk.jfr.Description;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+
+@Path("/world-bank")
+@Tag(name = "world-bank")
+public class WorldBankResource {
+
+    @Inject
+    @RestClient
+    WorldBankService worldBankService;
+
+    @GET
+    @Path("/{cod_pais}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(description = WorldBankConstant.GET_COD_PAIS)
+    @Tag(name="world-bank")
+    @APIResponse(responseCode = "404", description = WorldBankConstant.GET_COD_PAIS_STATUS_NOT_FOUND)
+    @APIResponse(responseCode = "200", description = WorldBankConstant.GET_COD_PAIS_STATUS_OK)
+    public Response findByCdPaisPaginator(@PathParam("cod_pais") String cod_pais, @QueryParam("page") String page, @QueryParam("per_page") String perPage, @QueryParam("format") String format)
+    {
+        this.checkParamsWorldBank(cod_pais, page, perPage, format);
+        ArrayList<Object> callback = worldBankService.findByCdPaisPaginator(cod_pais, page, perPage, format);
+        if(callback.isEmpty()) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        if(callback.size() == 1) {
+            return Response.status(Response.Status.NOT_FOUND).entity(WorldBankConstant.GET_COD_PAIS_STATUS_NOT_FOUND).build();
+        }
+        return Response.ok().entity(callback).build();
+    }
+
+    @Description(WorldBankConstant.DESCRIPTION_CHECK_PARAMS_NOT_NULL)
+    public void checkParamsWorldBank(String cod_pais, String page, String perPage, String format) {
+        if (cod_pais == null || page == null || perPage == null || format == null) {
+            throw new WebApplicationException(
+                    Response.status(Response.Status.BAD_REQUEST)
+                            .entity(WorldBankConstant.PARAMS_REQUIRED)
+                            .build()
+            );
+        }
+    }
+}
---
+++ backend/src/main/java/org/poc/restclient/worldbank/WorldBankService.java
@@ -0,0 +1,20 @@
+package org.poc.restclient.worldbank;
+
+import jdk.jfr.Description;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+
+@Path("/country")
+@RegisterRestClient
+public interface WorldBankService {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("{cod_pais}/indicator/SI.POV.DDAY")
+    @Description(WorldBankConstant.API_INFO_WORLD_BANK)
+    public ArrayList<Object> findByCdPaisPaginator(@PathParam("cod_pais") String cod_pais, @QueryParam("page") String page, @QueryParam("per_page") String perPage, @QueryParam("format") String format);
+
+}
